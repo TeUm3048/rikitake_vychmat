4d0194f8cad48d8927e6267cf4f439a524268671
Iwan Ryžij <teum3048@yandex.ru>
2024-06-03 17:40:59 +0300

    Update scripts and data files

--- scripts/calc_data_and_distance.py
+++ scripts/calc_data_and_distance.py
@@ -5,11 +5,16 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+import logging
+
+
 def remove_prefix(s, prefix):
     return s[len(prefix):] if s.startswith(prefix) else s
 
+
 def remove_suffix(s, suffix):
     return s[:-len(suffix)] if s.endswith(suffix) else s
+
 
 def create_data(solver: str, to: float, step: float = None, atol=None, rtol=None, every: float = None, filename: str = None):
     if not os.path.exists("data"):
@@ -26,6 +31,7 @@ def create_data(solver: str, to: float, step: float = None, atol=None, rtol=None
         if filename is None:
             filename = f"data/{solver}_{to}_{step}_{every}.csv"
         s = f"./main --solver {solver} --end {to} --step {step} {every_param} > {filename}"
+    logging.info("Running %s", s)
     subprocess.run(s, shell=True, check=True)
     return filename
 
@@ -40,8 +46,11 @@ def generate_data(solver: str, to: float, step: float = None, atol=None, rtol=No
         if filename is None:
             filename = f"data/{solver}_{to}_{step}_{every}.csv"
     if not os.path.exists(filename):
+        logging.info("Data does not exist in %s", filename)
         filename = create_data(solver=solver, to=to, step=step,
                                atol=atol, rtol=rtol, every=every, filename=filename)
+    else:
+        logging.info("Data already exists in %s", filename)
     return filename, pd.read_csv(filename).set_index("time")
 
 
@@ -60,39 +69,61 @@ def save_distance(data1, data2, lable1, lable2):
 
     # if not os.path.exists("data"):
     #     os.makedirs("data")
-    l1 = remove_suffix(lable1,'.csv')
-    l1 = remove_suffix(l1,'data/').replace('/', '_')
-    l2 = remove_suffix(lable2,'.csv')
-    l2 = remove_prefix(l2,'data/').replace('/', '_')
+    l1 = remove_suffix(lable1, '.csv')
+    l1 = remove_prefix(l1, 'data/').replace('/', '_')
+    l2 = remove_suffix(lable2, '.csv')
+    l2 = remove_prefix(l2, 'data/').replace('/', '_')
 
     filename = f"data/distance_{l1}_{l2}.csv"
     dis.to_csv(filename)
+    logging.info("Saved distance to %s", filename)
+    return filename
 
 
 def main():
+    logging.basicConfig(level=logging.INFO)
+
     dir_path = os.path.dirname(os.path.realpath(__file__))
 
-    os.chdir(dir_path + "/../..")
+    os.chdir(dir_path + "/..")
+
+    logging.info("Project dict: %s", os.getcwd())
 
     end_point = 20
 
     rk4_filename, rk4_data = generate_data(
         "RK4", to=end_point, step=0.0005, every=0.01)
+    logging.info("Calculated with RK4 and save data to %s", rk4_filename)
+
     dopri8_filename, dopri8_data = generate_data(
         "dopri8", to=end_point, step=0.01, every=0.01)
+    logging.info("Calculated with dopri8 and save data to %s", dopri8_filename)
+
     ab_filename, ab_data = generate_data(
         "ADAMS_BASHFORTH", to=end_point, step=0.01, every=0.01)
+    logging.info(
+        "Calculated with ADAMS_BASHFORTH and save data to %s", ab_filename)
+
     am_filename, am_data = generate_data(
         "ADAMS_MOULTON", to=end_point, step=0.0005, every=0.01)
+    logging.info(
+        "Calculated with ADAMS_MOULTON and save data to %s", am_filename)
+
     dopri54_filename, dopri54_data = generate_data(
         "DOPRI54", to=end_point, atol=1e-8, rtol=1e-5, every=0.01)
+    logging.info("Calculated with DOPRI54 and save data to %s",
+                 dopri54_filename)
 
     save_distance(rk4_data, dopri8_data, rk4_filename, dopri8_filename)
-    save_distance(rk4_data, ab_data, rk4_filename, ab_filename)
-    save_distance(rk4_data, am_data, rk4_filename, am_filename)
-    save_distance(rk4_data, dopri54_data, rk4_filename, dopri54_filename)
-    save_distance(dopri54_data, am_data, dopri54_filename, am_filename)
 
+    save_distance(rk4_data, ab_data, rk4_filename, ab_filename)
+
+    save_distance(rk4_data, am_data, rk4_filename, am_filename)
+
+    save_distance(rk4_data, dopri54_data,
+                  rk4_filename, dopri54_filename)
+
+    save_distance(dopri54_data, am_data, dopri54_filename, am_filename)
 
 
 if __name__ == "__main__":
--- scripts/create-data.sh
+++ scripts/create-data.sh
@@ -4,8 +4,8 @@ mkdir -p "$BASEDIR/../data"
 
 cd "$BASEDIR/.."
 
-./main --solver RK4 --end 20 --step 0.0005 --every 0.01 >data/RK4_20_0.0005_0.01.csv
-./main --solver DOPRI8 --end 20 --step 0.01 --every 0.01 >data/DOPRI8_20_0.01_0.01.csv
-./main --solver ADAMS_BASHFORTH --end 20 --step 0.01 --every 0.01 >data/ADAMS_BASHFORTH_20_0.01_0.01.csv
-./main --solver ADAMS_MOULTON --end 20 --step 0.0005 --every 0.01 >data/ADAMS_MOULTON_20_0.0005_0.01.csv
-./main --solver DOPRI54 --end 20 --atol 1e-08 --rtol 1e-05 --every 0.01 >data/DOPRI54_20_a1e-08_r1e-05_0.01.csv
+./main --solver RK4 --end 20 --step 0.0005 --every 0.01 >"data/RK4_20_0.0005_0.01.csv"
+./main --solver DOPRI8 --end 20 --step 0.01 --every 0.01 >"data/DOPRI8_20_0.01_0.01.csv"
+./main --solver ADAMS_BASHFORTH --end 20 --step 0.01 --every 0.01 >"data/ADAMS_BASHFORTH_20_0.01_0.01.csv"
+./main --solver ADAMS_MOULTON --end 20 --step 0.0005 --every 0.01 >"data/ADAMS_MOULTON_20_0.0005_0.01.csv"
+./main --solver DOPRI54 --end 20 --atol 1e-08 --rtol 1e-05 --every 0.01 >"data/DOPRI54_20_a1e-08_r1e-05_0.01.csv"
